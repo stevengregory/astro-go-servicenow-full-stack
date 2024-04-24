de7cd112eee7ce7a242ec8c6aee327dc8ec5f099
Steven Gregory <sgregory@gmail.com>
2024-04-24 16:37:30 -0500

    feat: integrate viper config

--- main.go
+++ main.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-resty/resty/v2"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	initConfig()
 	r := setupRouter()
 	r.Run(":8080")
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+
+	viper.AutomaticEnv()
 }
 
 func setupRouter() *gin.Engine {
@@ -21,18 +36,23 @@ func setupRouter() *gin.Engine {
 
 func fetchFromServiceNow(c *gin.Context) {
 	client := resty.New()
-	client.SetBasicAuth("user", "******")
+	username := viper.GetString("servicenow.username")
+	password := viper.GetString("servicenow.password")
+	baseURL := viper.GetString("servicenow.baseURL")
+
+	client.SetBasicAuth(username, password)
 	userQuery := c.DefaultQuery("filter", "active=true")
 	limit := c.DefaultQuery("limit", "10")
 	fields := c.DefaultQuery("fields", "active,assigned_to,number,short_description,priority,sys_id")
 
+	fullURL := baseURL + "/api/now/table/incident"
 	resp, err := client.R().
 		SetHeader("Accept", "application/json").
 		SetHeader("Content-Type", "application/json").
 		SetQueryParam("sysparm_query", userQuery).
 		SetQueryParam("sysparm_limit", limit).
 		SetQueryParam("sysparm_fields", fields).
-		Get("https://instance.service-now.com/api/now/table/incident")
+		Get(fullURL)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch data from ServiceNow"})
